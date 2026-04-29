4afa909ffbb6ee3a13dcd51b2dbe1ede03c731a6
farhat1926 <farhatmoh2000@gmail.com>
2026-04-29 13:55:27 +0700

    push lag

--- frontend/src/components/EventDetail.jsx
+++ frontend/src/components/EventDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import {link} from "react-router-dom";  
+import {Link} from "react-router-dom";  
 
 const EventDetail = () => {
   const { id } = useParams();
