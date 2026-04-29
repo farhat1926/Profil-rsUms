85e1935d6cab10e5cf935bb8c5e64859d84bbc4d
farhat1926 <farhatmoh2000@gmail.com>
2026-04-29 13:52:59 +0700

    push lagi8

--- frontend/src/components/DetailPromo.jsx
+++ frontend/src/components/DetailPromo.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 export default function DetailPromo() {
   const { id } = useParams();
   const navigate = useNavigate();
